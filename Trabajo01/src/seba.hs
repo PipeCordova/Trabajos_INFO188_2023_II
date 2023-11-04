e18b112f661b636b90174b6faad434a29d9c6ce8
SebaMonte07 <seb.monte17@gmail.com>
2023-11-04 19:56:08 -0300

    Verificar que la pos de jugador y tesoro no son iguales

--- Trabajo01/src/seba.hs
+++ Trabajo01/src/seba.hs
@@ -7,46 +7,65 @@ import Data.List.Split (chunksOf)
 data Celda = Caminable | Lava | Obstaculo | Tesoro | Personaje deriving (Eq)
 
 instance Show Celda where
-  show Caminable = " "
-  show Lava = "$"
-  show Obstaculo = "L"
-  show Tesoro = "X"
-  show Personaje = "@"
+    show Caminable = " "
+    show Lava = "$"
+    show Obstaculo = "L"
+    show Tesoro = "X"
+    show Personaje = "@"
 
 data Game = Game
-  { tamMapa :: Int,
+    {
+    tamMapa :: Int,
     posTesoro :: (Int, Int),
+    posPersonaje :: (Int, Int),
     mapa :: [[Celda]]
-  }
+    }
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let n = read (args !! 0) :: Int
-      s = read (args !! 1) :: Int
+    args <- getArgs
+    let n = read (args !! 0) :: Int
+        s = read (args !! 1) :: Int
 
-  let game = generateGame n s
+    let game = generateGame n s
 
-  printGame game
+    printGame game
 
 generateGame :: Int -> Int -> Game
 generateGame n s =
-  let gen = mkStdGen s
-      (x, gen') = randomR (0, n - 1) gen
-      (y, _) = randomR (0, n - 1) gen'
-      positions = [(i, j) | i <- [0 .. n - 1], j <- [0 .. n - 1]]
-      celdaList = map (\pos -> getCelda (mkStdGen (s * (posToSeed pos))) pos) positions
-      celdas = chunksOf n celdaList
-   in Game {tamMapa = n, posTesoro = (x, y), mapa = celdas}
+    let gen = mkStdGen s
+        positions = [(i, j) | i <- [0 .. n - 1], j <- [0 .. n - 1]]
+        celdaList = map (\pos -> getCelda (mkStdGen (s * (posToSeed pos))) pos) positions
+        (x, gen') = randomR (0, n - 1) gen
+        (y, gen'') = randomR (0, n - 1) gen'
+        (x',gen''') = checkPosition n (x, gen'')
+        (y',_) = checkPosition n (y, gen''')
+        celdas = agregaObjetos (x,y) Tesoro (chunksOf n celdaList)
+        celdas2 = agregaObjetos (x',y') Personaje celdas
+    in Game {tamMapa = n, posTesoro = (x, y), posPersonaje = (x',y'), mapa = celdas2}
+
+-- Función para verificar si dos posiciones son distintas y generar una nueva posición si no lo son.
+checkPosition :: Int -> (Int, StdGen) -> (Int, StdGen)
+checkPosition n (pos, gen) = 
+    let (pos', gen') = randomR (0, n - 1) gen
+    in if pos' /= pos
+        then (pos', gen')
+        else checkPosition n (pos, gen')
+
+
+-- agrega el personaje o tesoro en las posiciones aleatorias
+agregaObjetos :: (Int, Int) -> Celda -> [[Celda]] -> [[Celda]]
+agregaObjetos (x, y) objeto grid = take y grid ++ [take x (grid !! y) ++ [objeto] ++ drop (x+1) (grid !! y)] ++ drop (y+1) grid
 
 getCelda :: StdGen -> (Int, Int) -> Celda
 getCelda gen pos =
-    let (r, _) = randomR (0 :: Int, 30) gen
-        in if r == 0
+    let (r, _) = randomR (0 :: Int, 100) gen
+        in if r < 5
             then Obstaculo
-            else if r == 1
+            else if r < 10
                 then Lava
                 else Caminable
+
 
 -- Función para convertir una posición a una semilla para generar números aleatorios
 posToSeed :: (Int, Int) -> Int
@@ -54,9 +73,9 @@ posToSeed (x, y) = mod (2654435761 * x + 2654435769 * y) 1000000007
 
 printGame :: Game -> IO ()
 printGame game = do
-  putStrLn $ "Tamaño del mundo: " ++ show (tamMapa game)
-  putStrLn $ "Tesoro en la posición: " ++ show (posTesoro game)
-  putStrLn "Mapa:"
-  mapM_ (putStrLn . concatMap show) (mapa game)
+    putStrLn $ "Tamaño del mundo: " ++ show (tamMapa game)
+    putStrLn $ "Tesoro en la posición: " ++ show (posTesoro game)
+    putStrLn "Mapa:"
+    mapM_ (putStrLn . concatMap show) (mapa game)
 
 
